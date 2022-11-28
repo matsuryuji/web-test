c8bc485d78cc7557b123cf7d0e4a5c4552b178a0
matsuryuji <mrmatsutane@hotmail.com>
2022-11-27 22:43:30 -0300

    feat: create store and postsSlice

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,14 @@
-const Home = () => (
-     <div>uhsauhsauhsa</div>
-)
+import { useState } from "react";
+
+const Home = () => {
+  const [posts] = useState("sasas");
+
+  return (
+    <div>
+      {posts}
+    </div>
+  )
+}
 
 
 export default Home;
